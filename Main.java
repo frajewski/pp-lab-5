20ea246f4520035952aec7a99dca9500bb2e93f5
frajewski <vip171@vp.pl>
2024-05-04 11:45:58 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(2, 14);
-        System.out.println(result);
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Pawel Gawel", 15);
+            people[2] = new Person("Tomasz Lis", 44);
+            people[3] = new Person("Andrzej Duda", 54);
+            people[4] = new Person("Jerzy Urban", 62);
+        } catch (InvalidAgeException e) {
+            System.out.println("Age is invalid: " + e.getMessage());
+        }
 
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Wynik:"+result);
+
+        for (Person person : people) {
+            if (person != null) {
+                int value = MathUtils.add(person.getAge(), b);
+                emailMessenger.sendMessage(person.getName() + " value: " + value);
+            }
+        }
     }
 }
