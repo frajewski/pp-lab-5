962835f5879bad47be5cc7e82eb2c39d53cf2dd9
frajewski <vip171@vp.pl>
2024-05-04 11:43:55 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(2, 14);
+        System.out.println(result);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("message from email");
+        emailMessenger.sendMessage("Wynik:"+result);
     }
 }
