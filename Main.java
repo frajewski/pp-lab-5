ed999bb2621035ccd30c869d65131bcf182c3740
frajewski <vip171@vp.pl>
2024-05-04 11:37:01 +0200

    zad1

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-asa
+import company.beans.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Pawel", 19);
+            System.out.println("Name:" + person.getName());
+            System.out.println("Age:" + person.getAge());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        try {
+            Person person = new Person("sylwia", 211);
+            System.out.println("Name:" + person.getName());
+            System.out.println("Age:" + person.getAge());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+}
