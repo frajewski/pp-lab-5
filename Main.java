ecc411701255aaf3e0dd28cc896c8488af8532d3
frajewski <vip171@vp.pl>
2024-05-04 11:31:25 +0200

    zad1

--- Main.java
+++ Main.java
@@ -1,13 +1,1 @@
-import company.beans.Person;
-
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Person person = new Person("Tomek", 21);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-
-        try {
+asa
