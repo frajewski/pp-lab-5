630ee724dc4acf920aee30a919e202309315b246
Powerlifter2137 <keygle@protonmail.com>
2024-05-04 10:52:45 +0200

    zad1

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-Public class
+import company.beans.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Tomek", 21);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        try {
+            Person person = new Person("Piotr", 150);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+}
